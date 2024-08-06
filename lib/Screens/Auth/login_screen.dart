32ae298c69baa529e48a77c228b4863db17bfd1d
Naveen <kirmara09@gmail.com>
2024-08-06 11:41:44 +0530

    Add login Screen AppBar

--- lib/Screens/Auth/login_screen.dart
+++ lib/Screens/Auth/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bus_owner/main.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -5,6 +6,10 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: styleSheet.colors.primary,
+      ),
+    );
   }
 }
